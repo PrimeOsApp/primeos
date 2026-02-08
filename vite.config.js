2af845846582e307bfa9560c90c482c5286525b8
Prime Odontologia <andrestallion@gmail.com>
2026-02-08 12:17:46 -0300

    Format vite.config.js for better readability

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,4 @@
-import { defineConfig } from "vite"; import react from "@vitejs/plugin-react"; import base44 from "@base44/vite-plugin"; export default defineConfig({ plugins: [ react(), base44(), ], base: "/", // 👈 OBRIGATÓRIO });
+import { defineConfig } from "vite"; 
+import react from "@vitejs/plugin-react"; 
+import base44 from "@base44/vite-plugin"; 
+export default defineConfig({ plugins: [ react(), base44(), ], base: "/", // 👈 OBRIGATÓRIO });
